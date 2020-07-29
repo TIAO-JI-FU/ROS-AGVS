4aeaabf683d17ba15193802c9410b8fde7585c8c
TIAO-JI-FU-6057 <tiaojifu6057.gmail.com>
2020-07-29 16:53:27 +0800

    Update_README_and_agvs_topic_control

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # ROS-AGVS
+### Schedule Software
+No.| Features              | State    | Date   |
+:-:|:---------------------:|:--------:|:------:|
+1  | Flow chart            | Finish   |7/22    |
+2  | AGVS model            | Finish   |7/23    |
+3  | Goods model           | Finish   |7/23    |
+4  | Map model             | Finish   |7/24    |
+5  | AGVS controller       | Finish   |7/27    |
+6  | Read Lidar data       | Finish   |7/27    |
+7  | Move AGVS             | Finish   |7/28    |
+8  | SLAM map              | N/A      | N/A    |
+9  | Navigation            | N/A      | N/A    |
+10 | Location calculation  | N/A      | N/A    |
+11 | Angle calculation     | N/A      | N/A    |
+12 | Distance calculation  | N/A      | N/A    |
+13 | Imformation collection| N/A      | N/A    |
+14 | Instruction decode    | N/A      | N/A    |
+
+### Schedule Hardware
+No.| Features              | State    | Date   |
+:-:|:---------------------:|:--------:|:------:|
+1  | Robot move (Raspberry)| N/A      | N/A    |
+2  | Lidar                 | N/A      | N/A    |
+3  | Image recognition     | N/A      | N/A    |
+4  | Movement correction   | N/A      | N/A    |
+5  | Arduino control       | N/A      | N/A    |
+6  | Integration           | N/A      | N/A    |
+7  | Debug                 | N/A      | N/A    |
--- agvs_topic_control/src/agvs_topic_control.py
+++ agvs_topic_control/src/agvs_topic_control.py
@@ -3,20 +3,17 @@
 import rospy
 import math
 from std_msgs.msg import Float64,Bool
-linear = 0
-angular = 0
-def callback_linear(data):
-    global linear
-    linear = data.data
-def callback_angular(data):
-    global angular
-    if(data.data):
-        angular = 1.57
-    else:
-        angular = 0
 
-def listen_my_topic():
-    rospy.init_node('listen_my_topic', anonymous=True)
+start = False
+now_speed = 0.0
+now_rotate = 0.0
+job = [-3200,-2400,-1000,1600,3700,-850,-1000,1600]
+
+def callback_start(data):
+    global start
+    start = True
+
+def push_my_data():
     pub_fr_rotate = rospy.Publisher('agvs/front_right_motor_wheel_joint_controller/command', Float64, queue_size=10)
     pub_fl_rotate = rospy.Publisher('agvs/front_left_motor_wheel_joint_controller/command', Float64, queue_size=10)
     pub_br_rotate = rospy.Publisher('agvs/back_right_motor_wheel_joint_controller/command', Float64, queue_size=10)
@@ -27,40 +24,81 @@ def listen_my_topic():
     pub_br_move = rospy.Publisher('agvs/back_right_wheel_joint_controller/command', Float64, queue_size=10)
     pub_bl_move = rospy.Publisher('agvs/back_left_wheel_joint_controller/command', Float64, queue_size=10)
 
-    rospy.Subscriber("my_topic_distance", Float64, callback_linear)
-    rospy.Subscriber("my_topic_angle", Bool, callback_angular)
-    
-    rate = rospy.Rate(5)
-    now_speed = 0
-    now_rotate = 0
-    rate_speed = 0.2
-    rate_rotate = 0.157
-    while not rospy.is_shutdown():
-        global linear
-        global angular
-        if(now_speed == linear):
-            #linear = 0
-            pass
-        elif(linear > now_speed):
-            now_speed = now_speed + rate_speed
-        else:
-            now_speed = now_speed - rate_speed
+    global now_rotate
+    pub_fr_rotate.publish(float(now_rotate))
+    pub_fl_rotate.publish(float(now_rotate))
+    pub_br_rotate.publish(float(now_rotate))
+    pub_bl_rotate.publish(float(now_rotate))
 
-        if(angular == now_rotate):
-            pass
-        elif(angular > now_rotate):
-            now_rotate = now_rotate + rate_rotate
-        else:
-            now_rotate = now_rotate -rate_rotate
-        
-        pub_fr_rotate.publish(float(now_rotate))
-        pub_fl_rotate.publish(float(now_rotate))
-        pub_br_rotate.publish(float(now_rotate))
-        pub_bl_rotate.publish(float(now_rotate))
-        pub_fr_move.publish(float(now_speed))
-        pub_fl_move.publish(float(now_speed))
-        pub_br_move.publish(float(now_speed))
-        pub_bl_move.publish(float(now_speed))
+    global now_speed
+    pub_fr_move.publish(float(now_speed))
+    pub_fl_move.publish(float(now_speed))
+    pub_br_move.publish(float(now_speed))
+    pub_bl_move.publish(float(now_speed))
+
+def listen_my_topic():
+    rospy.init_node('listen_my_topic', anonymous=True)
+    rospy.Subscriber("callback_start", Bool, callback_start)
+    
+    rate = rospy.Rate(100)
+    
+    global now_speed
+    max_speed = 0.0
+    rate_speed = 0.05
+    
+    global now_rotate
+    max_rotate = 0.0
+    rate_rotate = math.pi/800
+
+    now_count_time = 0
+    max_count_time = 0
+
+    now_job_count = -1
+    next_job_count = 0
+
+    ang_or_spe = False
+    while not rospy.is_shutdown():
+        if(start):
+            if(now_job_count != next_job_count):
+                if(next_job_count >= len(job)):
+                    return 0
+                now_job_count = next_job_count
+                max_count_time = abs(job[now_job_count])
+                max_speed = (job[now_job_count]/abs(job[now_job_count]))*10.0
+                max_rotate = (now_job_count%2)*(math.pi/2)
+            if(now_count_time == max_count_time):
+                max_speed = 0.0
+
+            if(ang_or_spe == False):
+                if(abs(max_rotate - now_rotate)<0.00001):
+                    ang_or_spe = True
+                    now_count_time = 0
+                elif(max_rotate > now_rotate):
+                    now_rotate = now_rotate + rate_rotate
+                    push_my_data()
+                else:
+                    now_rotate = now_rotate -rate_rotate
+                    push_my_data()
+            else:
+                now_count_time+=1
+                if(abs(max_speed - now_speed)<0.00001):
+                    pass
+                elif(max_speed > now_speed):
+                    now_speed = now_speed + rate_speed
+                    push_my_data()
+                else:
+                    now_speed = now_speed - rate_speed
+                    push_my_data()
+
+            if((max_speed == 0.0) and (abs(max_speed - now_speed)<0.00001)):
+                ang_or_spe = False
+                rospy.sleep(2.0)
+                next_job_count+=1
+
+        print("-------------------------")
+        print("max_rotate: ",max_rotate)
+        print("now_rotate: ",now_rotate)
+        print("now_speed: ",now_speed)
         rate.sleep()
 
     rospy.spin()
